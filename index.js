42fd1b0b3b90c9ddb7da10261c5208117cb9ba72
Alexander Edwards <edwardsalexk@gmail.com>
2018-08-17 17:47:46 -0500

    changed output

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Digital Ocean!')
+  res.send('I have changed the message!')
 })
 
 app.listen(4000, function () {
