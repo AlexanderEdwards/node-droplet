e8b4cff6093881fac73db30a9dd44d27d15a19e4
Alexander Edwards <edwardsalexk@gmail.com>
2018-08-17 22:00:13 -0500

    and endpoint

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('App works.');
 })
 
+app.get('/endpoint', (req,res)=>{
+  res.send('this is an endpoint');
+})
+
 app.listen(4000, function () {
   console.log('Magic is happening on port 3000!')
 })
