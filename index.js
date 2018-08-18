d2542363a737b8ff06e0076c744a11f17a1c20a0
Alexander Edwards <edwardsalexk@gmail.com>
2018-08-17 23:49:21 -0500

    added crash

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var fs = require('fs');
 
 app.get('/', function (req, res) {
   res.send('App works.');
@@ -7,7 +8,15 @@ app.get('/', function (req, res) {
 
 app.get('/endpoint', (req,res)=>{
   res.send('this is an endpoint');
+  adfafasds();
   throw new Error("this is an error");
+})
+
+app.get('/crash', function (req, res) {
+  fs.readFile('somefile.txt', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+  });
 })
 
 app.listen(4000, function () {
