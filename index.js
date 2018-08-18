386acea1ce1b0c044ab4bde555b89b25eaf6ab85
Alexander Edwards <edwardsalexk@gmail.com>
2018-08-17 21:55:14 -0500

    app works

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('I have changed the message YEP BABY!');
+  res.send('App works.');
 })
 
 app.listen(4000, function () {
