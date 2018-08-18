6c35b141abaec326d920aa31f3a94c16e18d69fe
Alexander Edwards <edwardsalexk@gmail.com>
2018-08-18 15:52:33 -0500

    Added front end

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
 var fs = require('fs');
+var path = require('path');
+
+//Point to root of build directory 
+app.use(express.static('./test-app/dist/test-app'));
 
 //shredwards.live
 app.get('/', function (req, res) {
@@ -9,9 +13,7 @@ app.get('/', function (req, res) {
 
 //shredwards.live/endpoint
 app.get('/endpoint', (req,res)=>{
-  res.send('bu');
-  adfafasds();
-  throw new Error("this is an ");
+  res.sendFile(path.resolve('./test-app/dist/test-app/index.html'));
 })
 
 app.get('/crash', function (req, res) {
